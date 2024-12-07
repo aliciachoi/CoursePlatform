8735eefface23281f94399e8535e84dc96fbe86d
aliciachoi <aliciachoi2022@gmail.com>
2024-12-07 13:09:48 +0000

    Make migrations and migrate with Django

--- src/courses/models.py
+++ src/courses/models.py
@@ -11,10 +11,13 @@ class PublishStatus(models.TextChoices):
     COMING_SOON = "soon", "Coming Soon"
     DRAFT = "draft", "Draft"
 
+def handle_upload(instance, filename):
+    return f"{filename}"
+
 class Course(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
-    # uuid = models.UUIDField(default=uuid.uuid1, unique=True)
+    image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
     access = models.CharField(
         max_length=5, 
         choices=AccessRequirement.choices,
