8e42f290593a3abebb1d06a72770ad8e8df41284
aliciachoi <aliciachoi2022@gmail.com>
2024-12-07 11:56:21 +0000

    Initial commit

---
+++ src/courses/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+
+class AccessRequirement(models.TextChoices):
+    ANYONE = "any", "Anyone"
+    EMAIL_REQUIRED = "email", "Email required"
+
+class PublishStatus(models.TextChoices):
+    PUBLISHED = "publish", "Published"
+    COMING_SOON = "soon", "Coming Soon"
+    DRAFT = "draft", "Draft"
+
+class Course(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    # uuid = models.UUIDField(default=uuid.uuid1, unique=True)
+    access = models.CharField(
+        max_length=5, 
+        choices=AccessRequirement.choices,
+        default=AccessRequirement.EMAIL_REQUIRED
+    )
+    status = models.CharField(
+        max_length=10, 
+        choices=PublishStatus.choices,
+        default=PublishStatus.DRAFT
+        )
+
+    @property
+    def is_published(self):
+        return self.status == PublishStatus.PUBLISHED
+    
+
+"""
+- Lessons
+    - Title
+    - Description
+    - Video
+    - Status: Published, Coming Soon, Draft
+"""
+
+
