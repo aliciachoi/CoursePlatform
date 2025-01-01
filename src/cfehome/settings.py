3ce90d16c12ddcc26129010c4857315f3a73bd29
aliciachoi <aliciachoi2022@gmail.com>
2025-01-01 07:32:09 +0000

    URL view routing for database lookups

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party
+    "django_htmx",
+    "tailwind",
+    # internal
     "courses",
 ]
 
@@ -54,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = 'cfehome.urls'
--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("courses/", include("courses.urls")),
+    path("admin/", admin.site.urls),
 ]
 
 if settings.DEBUG:
